5d3af096d094f6b939fad05745227df892ddc975
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-12-06 12:44:07 +0600

    install jwt and create jwt token route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -29,16 +30,38 @@ async function run() {
         const mealsCollection = client.db("hosteldb").collection("meals")
         const userCollection = client.db("hosteldb").collection("users")
 
-        app.get('/meals',async (req,res)=>{
+        // middlewares
+        const verifyToken = (req,res,next)=>{
+            if(!req.headers.authorization){
+                return res.status(401).send({message:'forbiden access'})
+            }
+            const token = req.headers.authorization.split(' ')[1]
+            jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+                if(err){
+                    return res.status(401).send({message:'forbiden access'})
+                }
+                req.decoded = decoded;
+                next()
+            })
+        }
+
+        // jwt related api
+        app.post('/jwt', verifyToken, async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+            res.send({ token })
+        })
+
+        app.get('/meals', async (req, res) => {
             const result = await mealsCollection.find().toArray()
             res.send(result)
         })
 
-        app.get('/users',async (req,res)=>{
+        app.get('/users', async (req, res) => {
             const result = await userCollection.find().toArray()
             res.send(result)
         })
-        app.get(`/users/:id`,async (req,res)=>{
+        app.get(`/users/:id`, async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
             const result = await userCollection.findOne(query)
@@ -54,24 +77,24 @@ async function run() {
         app.post('/users', async (req, res) => {
             const newUser = req.body;
             // console.log(newProduct);
-            const query = {email: newUser.email}
+            const query = { email: newUser.email }
             const isUserExist = await userCollection.findOne(query)
-            if(isUserExist){
-                return res.send({message: "User already Exist"})
+            if (isUserExist) {
+                return res.send({ message: "User already Exist" })
             }
             const result = await userCollection.insertOne(newUser);
             res.send(result);
         })
 
-        app.patch(`/users/admin/:id`,async (req,res)=>{
+        app.patch(`/users/admin/:id`, async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)}
+            const filter = { _id: new ObjectId(id) }
             const updatedDoc = {
-                $set:{
-                    role:"admin"
+                $set: {
+                    role: "admin"
                 }
             }
-            const result = await userCollection.updateOne(filter,updatedDoc)
+            const result = await userCollection.updateOne(filter, updatedDoc)
             res.send(result);
         })
 
